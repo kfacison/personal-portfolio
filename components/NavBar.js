c31243f4e8bfb8704c6f7779803f16de42d55105
kfac <kfacison1998@gmail.com>
2024-02-05 16:43:08 -0600

    changed font

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,17 +2,17 @@
 // self-referencial links us the Link component
 import Link from "next/link"
 import "@/styles/globals.css"
-import { Mulish } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
-const mulish = Mulish({
+const montserrat = Montserrat({
     subsets: ['latin'],
-    weight: ['400','700'],
+    weight: ['400','500','600','700'],
 })
 
 export default function NavBar(){
     return (
         <>
-        <nav id="navBar" className={mulish.className}>
+        <nav id="navBar" className={montserrat.className}>
             &nbsp;
             <Link className="link" href="#">About</Link>
             &nbsp;
--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,14 @@
 import '@/styles/globals.css'
-import { Mulish } from 'next/font/google'
-import { Source_Code_Pro } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
-const mulish = Mulish({
+const montserrat = Montserrat({
   subsets: ['latin'],
-  weight: ['400','700'],
-})
-
-const SourceCodePro = Source_Code_Pro({
-  subsets: ['latin'],
-  weight: ['400','700'],
+  weight: ['400','500','600','700'],
 })
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={mulish.className}>
+    <main className={montserrat.className}>
       <Component {...pageProps} />
     </main>
   )
